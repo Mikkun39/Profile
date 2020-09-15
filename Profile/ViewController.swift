e102eeeca5de99f49f63265749b13c279d1edf29
Mizuki Takayama <miz39t@gmail.com>
2020-09-15 14:45:25 +0900

    Initial Commit

---
+++ Profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Profile
+//
+//  Created by 高山瑞基 on 2020/09/15.
+//  Copyright © 2020 Takayama Mizuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
